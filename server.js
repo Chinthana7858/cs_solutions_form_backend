098a4d31d5169819925244343076d3a666fe1724
Chinthana Prabhashitha <prabhashitharac.20@uom.lk>
2025-04-16 13:05:37 +0530

    connection_url

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT  
-});
+const connectionString = process.env.MYSQL_URL;
+
+const db = mysql.createConnection(connectionString);
 
 
 db.connect(err => {
